f578cc7ecdece17c4747a0dd8c72e8fe35f01c29
Joy Ame <104561447+JoyAme@users.noreply.github.com>
2024-06-21 21:05:46 +0100

    commit

--- src/pages/Loading/LazyLoad.jsx
+++ src/pages/Loading/LazyLoad.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LazyLoad = () => {
   return (
     <div className='text-center'>
-      <h1>LOADING...</h1>
+      <h1 className='text-xl'>LOADING...</h1>
     </div>
   )
 }
